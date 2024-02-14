8e1009bccd509be332e5fe57e3cc780c8d3a35d9
Joseph Sumabat <joseph@mercury.com>
2024-02-14 12:32:17 -0500

    Use default workspace config if available and no settings config

--- src/extension.ts
+++ src/extension.ts
@@ -6,13 +6,16 @@ import { makeClient } from './client'
 import { Config, LanguageConfig, alloglot } from './config'
 import { makeFormatter } from './formatter'
 import { makeTags } from './tags'
+import { readFileSync, writeFile, writeFileSync } from 'fs'
 
 export function activate(context: vscode.ExtensionContext): void {
   const settingsSection = vscode.workspace.getConfiguration(alloglot.root)
+  const alloglotWorkspaceLanguages = getWorkspaceConfig('.vscode/alloglot.json');
+  const alloglotVscodeLanguages = settingsSection.get<Array<LanguageConfig>>(alloglot.config.languages, [])
+  const alloglotLanguages = alloglotVscodeLanguages.length === 0 ? alloglotWorkspaceLanguages : alloglotVscodeLanguages;
 
   const config: Config = {
-    languages: settingsSection
-      .get<Array<LanguageConfig>>(alloglot.config.languages, [])
+    languages: alloglotLanguages
       .filter(lang => {
         // make sure no fields are whitespace-only
         // we mutate the original object because typescript doesn't have a `filterMap` function
@@ -50,6 +53,21 @@ export function activate(context: vscode.ExtensionContext): void {
     // ...and dynamically create completions, definitions, and code actions providers.
     ...config.languages.map(makeTags)
   )
+}
+
+function getWorkspaceConfig(workspaceConfigPath: string): LanguageConfig[] {
+  const workspaceFolders =  vscode.workspace.workspaceFolders?.map(folder => folder.uri)
+    try {
+      if (workspaceFolders && workspaceFolders.length > 0)
+      {
+        const fullPath = vscode.Uri.joinPath(workspaceFolders[0], workspaceConfigPath);
+        return JSON.parse(readFileSync(fullPath.path,'utf-8')).languages;
+      } else {
+        return []
+      }
+    } catch (err){
+      return []
+    }
 }
 
 export function deactivate() { }
